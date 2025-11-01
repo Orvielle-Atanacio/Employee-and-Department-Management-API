bbe93274204398d2e661c06fea4a266ed770c11f
Orvielle <orviellematanacio@gmail.com>
2025-11-01 15:30:21 +0800

    Testing of branch function in git :)

--- url-based-roles-rest-security-practice/src/main/java/com/luv2code/springboot/cruddemo/CruddemoApplication.java
+++ url-based-roles-rest-security-practice/src/main/java/com/luv2code/springboot/cruddemo/CruddemoApplication.java
@@ -8,5 +8,7 @@ public class CruddemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CruddemoApplication.class, args);
+
+        System.out.println("This is a test branch!");
     }
 }
